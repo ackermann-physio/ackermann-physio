14c962f6bec510f7b8fe1a4f72ba3a2675d176c8
awuerz <muell46@web.de>
2021-02-06 13:01:27 +0100

    Deleted example files

--- components/button.js
+++
@@ -1,3 +0,0 @@
-export default function Button({ children, ...pageProps })  {
-  return <span>This is a button: {children} </span>
-}
--- pages/konzept.js
+++ pages/konzept.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout'
 export default function About() {
   return (
     <Layout>
+      <h1>Unser Konzept</h1>
       And this is the main content.
     </Layout>
   )
