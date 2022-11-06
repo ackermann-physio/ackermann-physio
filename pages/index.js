9cf1fed9d9aa19e5301ab2dc338c68bb5d94c714
awuerz <78643563+awuerz@users.noreply.github.com>
2022-11-06 20:51:46 +0100

    Moved corona text to the bottom of the main page

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,6 @@ export default function About() {
     <Layout>
       <h1>Ackermann & Team <br /> Privatpraxis für Osteopathie & Physiotherapie</h1>
 
-      <div className={banner.wrapper}>
-        <span className={util.bold}>
-          Wir haben geöffnet <br />
-        </span>
-        <br />
-        Bitte nehmen Sie Ihre Termine wie gewohnt wahr. Wir sind von der Bundes- und Landesregierung angehalten, unsere Leistungen als Bestandteil der medizinischen Grundversorgung auch während der Corona-Krise zu erbringen.<br>
-        Bitte tragen Sie zum Betreten der Praxis und während der Behandlung eine FFP2-Maske. Sollten Sie nicht vollständig geimpft oder genesen sein, ist ein aktueller Test erforderlich.<br>
-        Wir versichern Ihnen, dass wir uns an die Hygienevorgaben des Robert Koch Instituts halten und unsere Mitarbeiter auf diesem Gebiet geschult sind. Unsere Räume werden intensiver als schon im Normalfall gereinigt und gelüftet. Raumluftfilter sind vorhanden und Desinfektionsmittel für den persönlichen Bedarf von Patienten und Therapeutinnen sind verfügbar. Alle Therapeutinnen tragen selbstverständlich eine FFP2-Maske.<br>
-      </div>
-
       <div className={[content.wrapper, content.largeImage].join(' ')}>
         <div className={content.text}>
           Wir freuen uns über Ihren Besuch und stellen uns gerne bei Ihnen vor. <br />
@@ -40,6 +30,16 @@ export default function About() {
           <img src="/img/physiotherapie_darmstadt_team2_600.jpg" />
           <span className={[content.subtitle, util.center].join(' ')}>Das Praxisteam</span>
         </div>
+      </div>
+
+      <div className={banner.wrapper}>
+        <span className={util.bold}>
+          Wir haben geöffnet <br />
+        </span>
+        <br />
+        Bitte nehmen Sie Ihre Termine wie gewohnt wahr. Wir sind von der Bundes- und Landesregierung angehalten, unsere Leistungen als Bestandteil der medizinischen Grundversorgung auch während der Corona-Krise zu erbringen.<br>
+        Bitte tragen Sie zum Betreten der Praxis und während der Behandlung eine FFP2-Maske. Sollten Sie nicht vollständig geimpft oder genesen sein, ist ein aktueller Test erforderlich.<br>
+        Wir versichern Ihnen, dass wir uns an die Hygienevorgaben des Robert Koch Instituts halten und unsere Mitarbeiter auf diesem Gebiet geschult sind. Unsere Räume werden intensiver als schon im Normalfall gereinigt und gelüftet. Raumluftfilter sind vorhanden und Desinfektionsmittel für den persönlichen Bedarf von Patienten und Therapeutinnen sind verfügbar. Alle Therapeutinnen tragen selbstverständlich eine FFP2-Maske.<br>
       </div>
 
       <div className={content.wrapper}>
